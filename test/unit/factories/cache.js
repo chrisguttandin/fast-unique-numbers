9014863562774846c4326890e5008f4ed6e464d9
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-09-07 15:34:45 +0200

    test: add tests for cache()

---
+++ test/unit/factories/cache.js
@@ -0,0 +1,54 @@
+import { createCache } from '../../../src/factories/cache';
+
+describe('cache()', () => {
+
+    let cache;
+    let lastNumberWeakMap;
+
+    beforeEach(() => {
+        lastNumberWeakMap = new WeakMap();
+
+        cache = createCache(lastNumberWeakMap);
+    });
+
+    describe('with a map', () => {
+
+        let map;
+
+        beforeEach(() => {
+            map = new Map();
+        });
+
+        it('should return the given number', () => {
+            expect(cache(map, 29)).to.equal(29);
+        });
+
+        it('should insert the map and the number to the lastNumberWeakMap', () => {
+            cache(map, 29);
+
+            expect(lastNumberWeakMap.get(map)).to.equal(29);
+        });
+
+    });
+
+    describe('with a set', () => {
+
+        let set;
+
+        beforeEach(() => {
+            set = new Set();
+        });
+
+        it('should return the given number', () => {
+            expect(cache(set, 29)).to.equal(29);
+        });
+
+        it('should insert the set and the number to the lastNumberWeakMap', () => {
+            cache(set, 29);
+
+            expect(lastNumberWeakMap.get(set)).to.equal(29);
+        });
+
+    });
+
+});
