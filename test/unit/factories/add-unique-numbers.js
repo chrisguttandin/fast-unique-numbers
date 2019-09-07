402a46d127a53153d621c6a3bdb0e767c2d7e397
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-09-07 15:07:57 +0200

    test: add tests for addUniqueNumber()

---
+++ test/unit/factories/add-unique-numbers.js
@@ -0,0 +1,30 @@
+import { createAddUniqueNumber } from '../../../src/factories/add-unique-number';
+import { stub } from 'sinon';
+
+describe('addUniqueNumber()', () => {
+
+    let addUniqueNumber;
+    let generateUniqueNumber;
+    let set;
+
+    beforeEach(() => {
+        generateUniqueNumber = stub();
+        set = new Set();
+
+        addUniqueNumber = createAddUniqueNumber(generateUniqueNumber);
+
+        generateUniqueNumber.returns(17);
+    });
+
+    it('should return the number returned by generateUniqueNumber()', () => {
+        expect(addUniqueNumber(set)).to.equal(17);
+    });
+
+    it('should add the number returned by generateUniqueNumber() to the given set', () => {
+        addUniqueNumber(set);
+
+        expect(set.size).to.equal(1);
+        expect(set.has(17)).to.be.true;
+    });
+
+});
