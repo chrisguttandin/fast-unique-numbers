4ae92ce03dc02848d485e4938364b72072b3776e
Christoph Guttandin <chrisguttandin@media-codings.com>
2026-06-10 23:02:45 +0200

    style: apply linting rules

--- config/vitest/expectation-node.ts
+++ config/vitest/expectation-node.ts
@@ -1,3 +1,4 @@
 import { defineConfig } from 'vitest/config';
 
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({ test: { bail: 1, dir: 'test/expectation/node/', include: ['**/*.js'], watch: false } });
